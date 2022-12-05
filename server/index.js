1ccd5c9fa4b96397e72199e105baa2aeaac06758
sujala Potula <sujalapotula1@gmail.com>
2022-12-04 22:53:03 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const { getCompliment } = require('./controller')
+
+app.get("/api/compliment", getCompliment);
+
+const { getFortune } = require('./controller')
+
+app.get("/api/fortune", getFortune);
+
+app.listen(5500, () => console.log("Server running on 5500"));
+
